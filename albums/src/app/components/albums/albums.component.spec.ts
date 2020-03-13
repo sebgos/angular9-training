65b52f51264949b8c2ed9d915bad83d081bfd24d
michalkotas <miko@mikofoto.net>
2020-03-13 15:05:47 +0100

    add unit tests

--- albums/src/app/components/albums/albums.component.spec.ts
+++ albums/src/app/components/albums/albums.component.spec.ts
@@ -1,6 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
+import { PhotosService } from 'src/app/services/photos.service';
 import { AlbumsComponent } from './albums.component';
+
+
+
+
+class PhotosServiceMock {
+   dummyPhotos = [
+    {
+      albumId: 1,
+      id: 1,
+      title: "title 1",
+      url: "http://url1.com",
+      thumbnailUrl: "http://thumbnail-url1.com"
+    },
+    {
+      albumId: 1,
+      id: 2,
+      title: "title 2",
+      url: "http://url2.com",
+      thumbnailUrl: "http://thumbnail-url2.com"
+    }
+  ];
+
+  getAlbums() {
+    return of(this.dummyPhotos)
+  }
+}
 
 describe('AlbumsComponent', () => {
   let component: AlbumsComponent;
@@ -8,7 +37,10 @@ describe('AlbumsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AlbumsComponent ]
+      declarations: [ AlbumsComponent ],
+      providers: [{
+        provide: PhotosService, useClass: PhotosServiceMock
+      }]
     })
     .compileComponents();
   }));
@@ -21,5 +53,13 @@ describe('AlbumsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render albums', () => {
+    component.ngOnInit();
+    fixture.detectChanges();
+    const aElements = fixture.debugElement.queryAll(By.css("a"));
+    expect(aElements.length).toEqual(2)
+    expect(aElements[0].nativeElement.text).toContain('title 1');
   });
 });
--- albums/src/app/services/photos.service.spec.ts
+++ albums/src/app/services/photos.service.spec.ts
@@ -1,16 +1,98 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpParams } from '@angular/common/http';
 
 import { PhotosService } from './photos.service';
 
+
 describe('PhotosService', () => {
   let service: PhotosService;
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PhotosService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PhotosService]
+    });
+    injector = getTestBed();
+    service = injector.get(PhotosService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('getAlbums', () => {
+    beforeEach(() => {
+
+    });
+
+    it('should return an Observable of albums', () => {
+      const dummyAlbums = [
+        {
+          userId: 1,
+          id: 1,
+          title: "title 1"
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "title 2"
+        },
+      ];
+
+      service.getAlbums().subscribe((albums: any) => {
+        expect(albums.length).toBe(2);
+        expect(albums).toEqual(dummyAlbums);
+      });
+
+      const req = httpMock.expectOne('https://jsonplaceholder.typicode.com/albums');
+      expect(req.request.method).toBe("GET");
+      req.flush(dummyAlbums);
+    });
+  });
+
+  describe('getPhotos', () => {
+    beforeEach(() => {
+
+    });
+
+    it('should return an Observable of photos', () => {
+      const dummyPhotos = [
+        {
+          albumId: 1,
+          id: 1,
+          title: "title 1",
+          url: "http://url1.com",
+          thumbnailUrl: "http://thumbnail-url1.com"
+        },
+        {
+          albumId: 1,
+          id: 2,
+          title: "title 2",
+          url: "http://url2.com",
+          thumbnailUrl: "http://thumbnail-url2.com"
+        }
+      ];
+      service.getPhotos(1).subscribe((photos: any) => {
+        expect(photos.length).toBe(2);
+        expect(photos).toEqual(dummyPhotos);
+      });
+
+
+
+      const req = httpMock.expectOne('https://jsonplaceholder.typicode.com/photos?albumId=1');
+
+      expect(req.request.method).toBe("GET");
+      expect(req.request.url).toBe(`https://jsonplaceholder.typicode.com/photos`);
+
+    });
+  });
+
 });
